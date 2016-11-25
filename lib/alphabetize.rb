538ec2b353f091ad37529f6e4989281190f127b0
Cathy Yee <catyyee@hotmail.com>
2016-11-25 01:55:10 +0000

    Done.

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,16 @@
+
+require 'pry'
+
 def alphabetize(arr)
-  # code here
+    esperanto = "abcĉdefgĝhĥijĵklmnopqrsŝtuŭvxywz".split("")
+    new_arr = []
+    arr.sort! # this alphabetizes based on second, sixth letters
+    esperanto.each do |char|  # abccde....
+      arr.each do |string|    # ["bonan matenon", "mi amas vin", "pacon", "ĉu vi parolas esperanton"]
+        if char == string[0]  # most of the words have been sorted. See rb:9. Only need to worry about the first letters with a caret
+          new_arr << string
+        end
+      end
+    end
+  new_arr
 end
